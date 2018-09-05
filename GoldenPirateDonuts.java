66f1bfdef96cecb54e12008432c76776779ba738
EddyWan <35242088+EddyWan@users.noreply.github.com>
2018-09-05 10:29:19 -0400

    Add files via upload

---
+++ GoldenPirateDonuts.java
@@ -0,0 +1,18 @@
+
+public class GoldenPirateDonuts {
+
+	public static void main(String[] args) {
+		int GourmetDonuts = 3 ;
+		double GoldDoubloons = 2.25 ;
+		double TreasureChest = GourmetDonuts*GoldDoubloons ;
+		double Pirates = TreasureChest*0.06 ;
+		double BlackBeard = Pirates*TreasureChest;
+		
+		
+		
+		
+		
+		
+	}
+
+}
